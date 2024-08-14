f227c9489d2b6a06fd77efc3a72e63f5321d02ec
sebastianduchene <sebastian.duchene6@gmail.com>
2024-08-14 11:50:15 +0200

    simulation replicates and code

--- temporal_sampling_bias/simulate_sampling_times.R
+++ temporal_sampling_bias/simulate_sampling_times.R
@@ -5,7 +5,7 @@
 library(NELSI)
 library(phangorn)
 
-setwd("~/Dropbox/projects_WORKING/temporal_signal_comment/simulation_scripts_remaster/")
+setwd("~/Dropbox/projects_WORKING/temporal_signal_comment/temporal_signal_comment/data_simulations/")
 
 simulated_trees <- list()
 
@@ -13,24 +13,23 @@ for(i in 1:100){
   # The sampling times will be exponentially distributed with a mean of 4000.
   # This means that on average the difference in sampling times is about 8,500, but can be about twice as high.
   sampling_times <- sort(round(rexp(5, 1/4000), 2))
-  max_time <- c(max_time, diff(range(sampling_times)))
-  
-  # This is biased sampling because older dates have a lower probability of being sampled
-  #sampled_data <- sort(sample(sampling_times, size = 100, replace = T, prob  =  (max(sampling_times) / sampling_times)))
-  # This is uniform sampling
-  sampled_data <- sort(sample(sampling_times, size = 100, replace = T)) 
+
+  #sampled_data <- sort(sample(sampling_times, size = 100, replace = T, prob  =  (max(sampling_times) / sampling_times))) # This is biased sampling because older dates have a lower probability of being sampled
+
+  sampled_data <- sort(sample(sampling_times, size = 100, replace = T)) # This is uniform sampling
+
   sampled_data <- sampled_data - min(sampled_data)
   sampling_summary <- table(sampled_data)
   
   times_for_xml <- paste(names(sampling_summary), collapse = " ")
   samples_for_xml <- paste(as.numeric(sampling_summary), collapse = " ")
   
-  xml_input <- readLines("~/Dropbox/projects_WORKING/temporal_signal_comment/simulation_scripts_remaster/coalescent_heterochronous.xml")
+  xml_input <- readLines("~/Dropbox/projects_WORKING/temporal_signal_comment/temporal_signal_comment/data_simulations/coalescent_heterochronous.xml")
   xml_input <- gsub("INPUT_TIMES", times_for_xml, xml_input)
   xml_input <- gsub("INPUT_NUM_SAMPLES", samples_for_xml, xml_input)
-  writeLines(xml_input, con = "test_out.xml")
+  writeLines(xml_input, con = "temp_out.xml")
   
-  system("~/phyloApps/beast2.7.6/bin/beast -overwrite test_out.xml")
+  system("~/phyloApps/beast2.7.6/bin/beast -overwrite temp_out.xml")
   
   tree_temp <- read.nexus("coal_trees.tree")
   tree_temp$tip.label <- paste(tree_temp$tip.label, round(allnode.times(tree_temp, tipsonly = T, reverse = F), 3), sep = "_")
@@ -48,6 +47,7 @@ for(i in 1:100){
   segment4 <- simSeq(phylogram_temp, l = 800, rate = site_rates[4])
   simulated_genome <- as.DNAbin(c(segment1, segment2, segment3, segment4))
   write.dna(simulated_genome, file = paste0("balanced_sampling_genome_", i, ".fasta"), format = "fasta", nbcol = -1, colsep = "")
+  plot(tree_temp, show.tip.label = F)
 }
 
 class(simulated_trees) <- "multiPhylo"
